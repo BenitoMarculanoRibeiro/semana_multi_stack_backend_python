d819224037ddf837936c71ae29544070f07af194
BenitoMarculanoRibeiro <benito2016mr@gmail.com>
2022-06-05 10:33:57 -0300

    0.0.1 - Colocando o projeto separado.

---
+++ adocao/views.py
@@ -0,0 +1,20 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
+from .serializers import AdocaoSerializer
+
+
+class AdocaoList(APIView):
+    def post(self, request, format=None):
+        serializer = AdocaoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=HTTP_200_OK)
+
+        return Response(
+            {
+                "erros": serializer.errors,
+                "message": "Houveram  erros de validação",
+            },
+            status=HTTP_400_BAD_REQUEST
+        )
